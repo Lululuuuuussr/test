4099d8f820765a21a69b535502292db3acd91d3f
fulu.tmp <fulu.tmp@vipkid.com.cn>
2019-12-25 17:20:57 +0800

    change

---
+++ webpack-demo/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition name="el-zoom-in-center">
+    <div class="wrapper">
+        <Aside></Aside>
+      <el-container>
+        <Header></Header>
+      </el-container>
+      
+    </div>  
+  </transition>
+</template>
+  
+<script>
+// import Home from './pages/home.vue'
+import Aside from './_aside/aside.vue'
+import Header from './_header/header.vue'
+
+export default {
+  name:'App',
+
+  components:{
+    Aside,
+    Header
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+.wrapper 
+  display flex
+  background #999
+  
+    
+
+</style>
+
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,10 +1,70 @@
 const path = require('path')
-const HtmlWebpackPlugin = 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
+  // 入口 
   entry:'./src/main.js',
+  // 插件
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:'./public/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin()
+  ],
+  // 输出
   output:{
-    filename:'build.js',
+    filename:'bundles.js',
     path:path.resolve(__dirname,'dist')
+  },
+  devServer:{
+    contentBase: './dist',
+    port: 8000
+  },
+  module:{
+    rules:[
+      {
+          test: /\.css$/,
+          use: [
+                'style-loader',
+                'css-loader'
+            ]
+      },
+      {
+          test: /\.(jpg|png|svg|gif)$/,
+          use: [
+              'file-loader'
+          ]
+      },
+      {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+              'file-loader'
+          ]
+      },
+      {
+          test: /\.js$/,
+          use: [
+              'babel-loader'
+          ],
+          exclude: /node_modules/
+      },
+      {
+          test: /\.vue$/,
+          use: [
+                'vue-loader'
+            ],
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      }
+    ]
   }
 }
